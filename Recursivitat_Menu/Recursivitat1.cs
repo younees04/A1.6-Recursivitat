aaa1bb2c18f2d7ad1a0add368253847f6ce71a71
Mohamed El Mhamdi <mohamed.elmhamdi004@gmail.com>
2024-01-29 17:38:44 +0100

    Llista 1 MOHA

--- Recursivitat_Menu/Recursivitat1.cs
+++ Recursivitat_Menu/Recursivitat1.cs
@@ -58,23 +58,6 @@ namespace Recursivitat_Menu
             return resultat;
         }
 
-        // 4
-        public static int DivisioEntera(int num1, int num2)
-        {
-            int quocient = 0;
-            if (num1 < num2)
-            {
-                quocient = 0;
-            }
-
-            else
-            {
-                quocient = 1 + DivisioEntera(num1 - num2, num2);
-            }
-
-            return quocient;
-        }
-
         //5
 
         public static int Modul(int a, int b)
@@ -130,5 +113,95 @@ namespace Recursivitat_Menu
             }
             return resultat;
         }
+
+        // MOHA
+        // 4.- Fer una funció que faci la divisió entera entre dos números 
+        public static int DivisioEntera(int num1, int num2)
+        {
+            int quocient = 0;
+            if (num1 < num2)
+            {
+                quocient = 0;
+            }
+
+            else
+            {
+                quocient = 1 + DivisioEntera(num1 - num2, num2);
+            }
+
+            return quocient;
+        }
+
+        // 6.- Fer una funció que retorni el valor màxim d’un vector d’enters. 
+        public static int ValorMaxim(int[] vector, int posicio)
+        {
+            int max = 0;
+
+            if (posicio == 0)
+            {
+                max = vector[posicio];
+            }
+            else
+            {
+                int maxAnterior = ValorMaxim(vector, posicio - 1);
+                max = Math.Max(vector[posicio], maxAnterior);
+            }
+
+            return max;
+        }
+
+        // 8.- Dissenyeu un algorisme recursiu que donat un array d’enters i un enter x,
+        // torni la posició on es troba x dins del array(-1 si no hi és).
+
+        public static int TrobaPosicio(int[] array, int valor, int posicio)
+        {
+            int pos = -1;
+
+            if (posicio == 0)
+            {
+                if (array[posicio] == valor)
+                {
+                    pos = posicio;
+                }
+            }
+
+            else
+            {
+                if (array[posicio] == valor)
+                {
+                    pos = posicio;
+                }
+                else
+                {
+                    pos = TrobaPosicio(array, valor, posicio - 1);
+                }
+            }
+
+            return pos;
+        }
+
+        // 10.- Fer una funció que ens digui si un nombre está en base b
+        public static bool EsBase(int num, int b)
+        {
+            bool esBase = false;
+
+            if (num == 0)
+            {
+                esBase = true;
+            }
+
+            else
+            {
+                if (num % b == 0)
+                {
+                    esBase = EsBase(num / b, b);
+                }
+            }
+
+            return esBase;
+        }
+
+
+
     }
 }
